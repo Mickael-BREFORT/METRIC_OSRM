765faacc35bcb78e527bfbb0d7670db05663adee
Mickael-BREFORT <mickael.brefort@insee.fr>
2024-03-27 10:31:56 +0000

    installation du package metric

--- 00_nouveau_programme.R
+++ 00_nouveau_programme.R
@@ -1,3 +1,36 @@
 
 
 # Création d'un programme simple pour former Caro au package Metric
+
+# -----------------------------------------#
+# METRIC - Accessibilité aux équipements----
+#                                          #
+#       Liste des packages nécessaires     #       
+# -----------------------------------------#
+
+
+install.packages("remotes")
+
+# Installation du package Metric
+remotes::install_gitlab(repo = 'metric-osrm/metric-osrm-package',
+                        host = 'git.lab.sspcloud.fr',
+                        upgrade = 'never',
+                        build = TRUE,
+                        dependencies = TRUE,
+                        force = TRUE)
+
+#-------------------------------------------
+
+library(tidyverse)
+library(metric.osrm) 
+
+# Adresse du serveur, ici le serveur expérimental déployé sur la plateforme 
+# du SSPCloud
+options(osrm.server = "https://metric-osrm-backend.lab.sspcloud.fr/") 
+
+# On précise le profil de routage, ici "driving" car le serveur 
+# SSPCloud ne propose que du calcul de trajet routier en voiture
+options(osrm.profile = "driving") 
+
+#-------------------------------------------
+
